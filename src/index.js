ced5cd2941e66aa247f6fd1765e2e8b1af82cad7
tasnimchy96 <tasnim.achy96@gmail.com>
2023-09-09 12:21:50 +0600

    Store and get data from api

--- src/index.js
+++ src/index.js
@@ -1,49 +1,31 @@
 import './style.css';
+import {
+  updateScore,
+  loadScore,
+  resetForm,
+  createGame,
+} from './modules/api.js';
 
-const nameInput = document.getElementById('name');
-const scoreInput = document.getElementById('score');
-const submitButton = document.getElementById('submit-btn');
-const ul = document.querySelector('.scores');
-
-// Retrieve scores from local storage and display them
-function displayScores() {
-  ul.innerHTML = ''; // Clear the list before adding scores
-
-  const scores = JSON.parse(localStorage.getItem('scores')) || [];
-
-  scores.forEach((score) => {
-    const li = document.createElement('li');
-    li.innerText = score;
-    ul.appendChild(li);
-  });
-}
-
-// Add a click event listener to the submit button
-submitButton.addEventListener('click', (event) => {
+const handleRefreshButtonClick = (event) => {
   event.preventDefault();
+  loadScore();
+};
 
-  const name = nameInput.value;
-  const score = scoreInput.value;
+const handleSubmitButtonClick = (event) => {
+  event.preventDefault();
+  const playerName = document.querySelector('#name').value;
+  const playerScore = document.querySelector('#score').value;
 
-  // Create a string representation of the score
-  const scoreString = `${name}:${score}`;
+  updateScore(playerName, playerScore);
+  resetForm();
+};
 
-  // Retrieve existing scores from local storage or create an empty array
-  const scores = JSON.parse(localStorage.getItem('scores')) || [];
+document
+  .querySelector('#refresh-btn')
+  .addEventListener('click', handleRefreshButtonClick);
+document
+  .querySelector('#submit-btn')
+  .addEventListener('click', handleSubmitButtonClick);
 
-  // Add the new score to the array
-  scores.push(scoreString);
-
-  // Store the updated scores array in local storage
-  localStorage.setItem('scores', JSON.stringify(scores));
-
-  // Display the scores
-  displayScores();
-
-  // Clear input fields
-  nameInput.value = '';
-  scoreInput.value = '';
-});
-
-// Initial display of scores when the page loads
-displayScores();
+createGame();
+loadScore();
